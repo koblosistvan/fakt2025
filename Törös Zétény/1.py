1c87ff7a640e3771676a8f9c4e685786e3a4a04f
Tejbep <toros.zeteny@gmail.com>
2025-09-17 08:57:27 +0200

    abc

--- Törös Zétény/1.py
+++ Törös Zétény/1.py
@@ -1,1 +1,8 @@
-sigma
+jelszó = 'pityputy'
+
+felhasználó_jelszó = input('Mi a jelszó? ')
+
+if jelszó == felhasználó_jelszó:
+    print('Hozzáférés megadva.')
+else:
+    print('A jelszó hibás, hozzáférés megtagadva.')
