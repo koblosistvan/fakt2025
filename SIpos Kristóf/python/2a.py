4834c4f667ca797310c852c363b18bd55af02aad
Sipos Kristóf <kristof.sipos@gmail.com>
2025-09-17 08:22:54 +0200

    jó lesz

---
+++ SIpos Kristóf/python/2a.py
@@ -0,0 +1,5 @@
+jelszó=input("Add meg a jelszót: ")
+if jelszó==("Aa123456"):
+    print("üdvözlöm")
+else:
+    print("Hozzáférés megtagadva")
---
+++ SIpos Kristóf/python/2d.py
@@ -0,0 +1,11 @@
+helyezet=int(input("Versenyen való helyezésed: "))
+if helyezet==1:
+    print('Arany lettél GG')
+elif helyezet==2:
+    print('Ezüst még mindig szép eredmény')
+elif helyezet==3:
+    print('Még mindig dobogós')
+elif helyezet >= 4:
+    print('Ez most nem nyert')
+else:
+    print('Are you serious right now bruh?')
