939c26d6bb0868a03b5d2dfacd5ae7f9ca670c3a
dbalint618 <dbalint618@gmail.com>
2025-09-17 08:26:15 +0200

    .-

---
+++ _Feladatok/python/Dobai Balint/python/2d.py
@@ -0,0 +1,10 @@
+hely = int(input('hanyadik lettel?:'))
+
+if hely == 1:
+    print('arany érem')
+elif hely ==2:
+    print('ezust erem')
+elif hely ==3:
+    print('bronz erem')
+else:
+    print('nem dobogos')
