d82e6ce7e93601b57ce2a936cfa3b338bbe1fafb
odora23 <odorartur09@gmail.com>
2025-09-22 17:51:54 +0200

    lowkey

---
+++ Ódor Artúr/python/3k.py
@@ -0,0 +1,15 @@
+szam = int(input("Adj meg egy számot: "))
+osztokszama = 0
+for i in range(1, szam + 1):
+    
+    if szam % i == 0:
+        osztokszama += 1
+    elif osztokszama > 2:
+        break
+    
+if osztokszama > 2:
+    print("Nem prím")
+elif szam == 1:
+    print("Egyesszám")
+else:
+    print("Prím")
