90c4694a2f613315880567ab77d68b1bed615a1a
RobozHunor <robozhunor@gmail.com>
2025-09-22 22:08:48 +0200

    hell yea

--- Roboz Hunor/python/3l.py
+++ Roboz Hunor/python/3l.py
@@ -3,13 +3,15 @@
 a = int(input("Add meg egy  számot: "))
 b = int(input("Add meg még egy számot: "))
 
+a2 = a
+b2 = b
 
-while b != 0:
-    maradek = a % b
-    a = b
-    b = maradek
+while b2 != 0:
+    maradek = a2 % b2
+    a2 = b2
+    b2 = maradek
 
-lnko = a
+lnko = a2
 lkkt = a * b // lnko
 
 print(lnko)
