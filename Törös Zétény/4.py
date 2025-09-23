df8f27b45e1ea529769dc7dce23f69188d1c2eea
Tejbep <toros.zeteny@gmail.com>
2025-09-23 13:38:48 +0200

    .

--- Törös Zétény/4.py
+++ Törös Zétény/4.py
@@ -1,9 +1,23 @@
-szám = int(input("Adj meg egy számot: "))
-alap = int(input("Add meg a számrendszer alapját: "))
-eredmény = ''
+p = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
 
-while szám > 0:
-    maradék = szám % alap
-    eredmény = str(maradék) + eredmény
-    szám = (szám - maradék) // alap
-print(f"Kettes számrendszerben: {eredmény}")
+print(p)
+print(p[1])
+print(f'Az első érték: {p[0]}')
+
+print(f'Az utolsó érték: {p[-1]}')
+
+print(p[1:3])
+print(p[1:])
+print(p[:3])
+
+print(p[1:5:2])
+
+szöveg = 'Még nyilnak a völgyben a kerti virágok még zöldel a nyárfa az ablak elött.'
+print(szöveg)
+print(szöveg[0:5])
+print(szöveg[::-1])
+
+if 5 in p:
+    print('Benne van')
+else:
+    print('Nincs benne')
