af21e6cbc08bb29f286e0ce41270700d035d736a
dbalint618 <dbalint618@gmail.com>
2025-10-08 09:49:19 +0200

    6-1

--- Dobai Balint/python/6a.py
+++ Dobai Balint/python/6a.py
@@ -7,6 +7,8 @@ kezdes_eve = []
 vegzes_eve = []
 adatok_szama =int(forras .readline())
 
+diakok_szama = len(szul_ev)
+
 
 for sor in forras:
     adat = sor.strip().split(' ')
@@ -24,3 +26,40 @@ for i in range(adatok_szama):
         break
 else:
     print('nincs ilyen ember')
+
+bek_ev = int(input('melyik evben szulettel?'))
+bek_honap = int(input('even belul melyik honapban szulettel?'))
+bek_nap = int(input('melyik napon szulettel?'))
+
+
+for i in range(adatok_szama):
+    if bek_ev == szul_ev[i] and bek_honap == szul_honap[i] and bek_nap == szul_nap[i]:
+        print('van a tieddel megegyezo szulinap')
+        break
+else:
+    print('nincs ilyen szulinap')
+
+
+legfiatalabb = vegzes_eve[0] - szul_ev[0]
+legfigatalabb_index = 0
+
+# for i in range(adatok_szama):
+#     if legfiatalabb > vegzes_eve[i] - szul_ev[i]:
+#          legfiatalabb = vegzes_eve[i] - kezdes_eve[i]
+#          legfigatalabb_index = i
+
+# print(f'a legfiatalabban vegzett hallgato {legfigatalabb_index}, {legfiatalabb} evesen')
+
+
+vmikor_vegzett = 0
+for i in range(adatok_szama):
+    if vegzes_eve[i] == 2016:
+        vmikor_vegzett +=1
+print(f'{vmikor_vegzett} tanulo vegzett 2016-ban')
+
+
+
+#hazi 2023/24 okt 1 fordulo programozoi
+
+
+
