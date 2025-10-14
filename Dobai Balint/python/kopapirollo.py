def520d5a98625f3fc65874e07e98d7f43529da3
dbalint618 <dbalint618@gmail.com>
2025-10-14 13:36:16 +0200

    salam aleykum

--- Dobai Balint/python/kopapirollo.py
+++ Dobai Balint/python/kopapirollo.py
@@ -7,26 +7,31 @@ gep_pontja = 0
 
 tied = str(input('valassz egyet ko papir es ollo kozul: '))
 
-
 for i in range(3):
     mutatas.append (random.randint (1,3))
 
-if mutatas == 1:
-    mutatas = 'ko'
-elif mutatas == 2:
-    mutatas = 'papir'
-else:
-    mutatas == 3
-    mutatas = 'ollo'
+    if mutatas == 1:    
+        mutatas = 'ko'
+    elif mutatas == 2:
+        mutatas = 'papir'
+    else:
+        mutatas == 3
+        mutatas = 'ollo'
 
 if tied == 'ko' and mutatas == 'papir' or tied == 'papir' and mutatas == 'ollo' or tied == 'ollo' and mutatas == 'ko':
-    print('vesztettel')
-    gep_pontja +=1
-
-
+        print('vesztettel')
+        gep_pontja +=1
+        
 if tied == 'ko' and mutatas == 'ollo' or tied == 'papir' and mutatas == 'ko' or tied == 'ollo' and mutatas == 'papir':
-    print('nyertel')
-    te_pontod +=1
+        print('nyertel')
+        te_pontod +=1
+        
+if tied == mutatas:
+            te_pontod +=1
+            gep_pontja +=1
+            
+
+ 
 
 
 if tied == mutatas:
@@ -35,9 +40,12 @@ if tied == mutatas:
 
 
 
+
+
 print(f'a gep ezt mutatta {mutatas}')
-print(te_pontod)
-print(gep_pontja)
+print(f'pontjaid szama {te_pontod}')
+print(f'gep pontjainak szama {gep_pontja}')
+
 
 
 
