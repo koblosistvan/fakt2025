709c8f6bcbbe0faee3e45b1ee90ba19152cc79ef
dbalint618 <dbalint618@gmail.com>
2025-10-15 09:49:33 +0200

    portugal magyar 2-2

--- Dobai Balint/python/6b.py
+++ Dobai Balint/python/6b.py
@@ -9,12 +9,11 @@ ido = []
 
 
 
-
 for sor in forras:
     adat = sor.strip().split(' ')
     hely.append(int(adat[0]))
-    ora.append(int(adat[0]))
-    perc.append(int(adat[0]))
+    ora.append(int(adat[1]))
+    perc.append(int(adat[2]))
     ido.append(int(adat[1])*60 + int(adat[2]))
 forras.close()
 
@@ -33,6 +32,54 @@ kezdete = f'{ora[leghosszabb_index]:0>2d} : {perc[leghosszabb_index] :0>2d}'
 vege = f'{ora[leghosszabb_index+1]:0>2d} : {perc[leghosszabb_index+1]:0>2d}'
 
 print(f'a leghosszabb itervallum kezdete: {kezdete}, a vege pedig {vege}')
+
+otvenhez = 0
+for i in range(meresek):
+    if hely[i] == 50:
+        otvenhez +=1
+
+print(f'az 50-es meresi ponthoz {otvenhez} ertek tartozik')
+
+
+bekert_ora = int(input('hany orakor? : '))
+bekert_perc = int(input('hany perckor? : '))
+for i in range(meresek):
+    if bekert_ora == ora[i] and bekert_perc == perc[i]:
+        print('a megadott idopontban volt meres')
+        break
+else: 
+    print('nincs a megadott idopontban meres')
+
+ket_meres_index = 0
+
+for i in range(meresek):
+    if ido[i] == ido[i-1]:
+        ket_meres_index +=1
+
+print(f'{ket_meres_index} alkalommal volt olyan amikor 2 meres tortent')
+
+
+
+szamlalok = [0] * 101
+for i in range(meresek):
+    szamlalok[hely[i]] += 1
+print(szamlalok)
+
+
+
+legtobb_meres = szamlalok[0]
+legtobb_meres_index = 0
+
+
+
+for i in range(len(szamlalok)):
+    if szamlalok[i-1] < szamlalok[i]:
+        szamlalok[i] = legtobb_meres
+        legtobb_meres_index += 1
+print(f'a legtobb meres {legtobb_meres}, melyet {legtobb_meres_index}-nel mertek')
+
+
+        
 
 
 
