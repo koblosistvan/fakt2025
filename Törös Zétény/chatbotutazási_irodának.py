aaa7e4ff72ea7d92f24d68b57de8b7257a796e4f
Tejbep <toros.zeteny@gmail.com>
2025-11-05 09:51:37 +0100

    charlie kirk

---
+++ Törös Zétény/chatbotutazási_irodának.py
@@ -0,0 +1,5 @@
+hova = input('Hava szeretnél utazni: ')
+print(f"Természetesen van utunk {hova} felé.")
+menyi = int(input("Mennyi pénzed van az útra: "))
+ár = hova/3*2
+print(f"Az utazás ára {ár:.0} Ft.")
