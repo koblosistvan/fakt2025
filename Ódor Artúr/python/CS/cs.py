271cf3296f88d5806f730881b7b9e4c8993b93bf
odora23 <odorartur09@gmail.com>
2025-11-25 13:36:04 +0100

    meccs

---
+++ Ódor Artúr/python/CS/cs.py
@@ -0,0 +1,21 @@
+forras = open("_Feladatok\\python\\CS\\cs.csv", mode="r", encoding="utf-8")
+idopont = []
+palya = []
+csapat = []
+pont = []
+pont_kulonbseg = []
+eredmeny = []
+forras.readline()
+for sor in forras:
+    adat = sor.strip().split(";")
+    idopont.append(adat[0])
+    palya.append(adat[1])
+    csapat.append(adat[2])
+    pont.append(int(adat[3]))
+    pont_kulonbseg.append(int(adat[4]))
+    eredmeny.append(adat[5])
+forras.close()
+print(f"{len(csapat) / 2:.0f} meccs adatai vannak az állományban")
+
+for a in idopont:
+    b = a.strip().split(" ")
