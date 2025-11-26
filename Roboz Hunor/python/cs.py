e3890b5b82b4379535839c746bd23ce7cfda29ae
RobozHunor <robozhunor@gmail.com>
2025-11-26 08:38:06 +0100

    a

--- Roboz Hunor/python/cs.py
+++ Roboz Hunor/python/cs.py
@@ -1,6 +1,6 @@
-forras = open('_Feladatok\\python\\CS\\cs.csv', mode='r',encoding='utf-8')
+forras = open('fakt2025\\_Feladatok\\python\\CS\\cs.csv', mode='r',encoding='utf-8')
 forras.readline()
-idő,pálya,csapat,pont,kölünbség,eredmény = [],[],[],[],[],[]
+idő,pálya,csapat,pont,különbség,eredmény = [],[],[],[],[],[]
 
 for sor in forras:
     adat = sor.strip().split(';')
@@ -8,14 +8,15 @@ for sor in forras:
     pálya.append(adat[1])
     csapat.append(adat[2])
     pont.append(int(adat[3]))
-    kölünbség.append(int(adat[4]))
+    különbség.append(int(adat[4]))
     eredmény.append(adat[5])
 forras.close()
 
 print(f'{int(len(csapat)/2)} meccs adatai vannak az állományban')
 
-elso = idő[0] 
-for i in range(len(idő)):
-    if idő[i] > elso:
-        elso = idő[i]
-print(f'Az első meccs {elso}-ben volt')
+for i in range(len(különbség)):
+    for k in range(len(különbség-i-1)):
+        if különbség[k] < különbség[k+1]:
+            különbség[k],különbség[k+1] = különbség[k+1],különbség[k]
+
+
