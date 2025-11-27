88435f695c10bcd539b0e2df2ffa89385ad46f4a
RobozHunor <robozhunor@gmail.com>
2025-11-27 14:56:13 +0100

    a

--- Roboz Hunor/python/bankszámla_roboz.py
+++ Roboz Hunor/python/bankszámla_roboz.py
@@ -1,59 +1,43 @@
-számla = [245,-60,150.8,2350,-125.5,-98,-311,100,-28,-185,555.6,600,-45.3,-100,-36.9,50,-895,-225,120,215.5]
-von = 0
-u = 0
-for  i in range(len(számla)):
-    if számla[i] > 0:
-        u +=1 
-    elif számla[i] < 0:
-        von += 1
-print(f'{u} alkalommal utaltak és {von} alkalommal vontak le pénzt a számláról')
+szamla = [245,-60,150.8,2350,-125.5,-98,-311,100,-28,-185,555.6,600,-45.3,-100,-36.9,50,-895,-225,120,215.5]
+osszeg = 0
+for i in range(len(szamla)):
+    osszeg += szamla[i]
+print("Összeg:", osszeg)
 
+terhelesek_szama = 0
+for i in range(len(szamla)):
+    if szamla[i] < 0:
+        terhelesek_szama += 1
+print("Terhelések száma:", terhelesek_szama)
 
-ö = 0
-for  i in range(len(számla)):
-    ö+=számla[i]
-print(f'{ö} euro van a számlán')
+legnagyobb_ertek = szamla[0]
+for i in range(len(szamla)):
+    if szamla[i] > legnagyobb_ertek:
+        legnagyobb_ertek = szamla[i]
+print("Legnagyobb érték:", legnagyobb_ertek)
 
-a = 0
+pozitiv_ertekek = []
+for i in range(len(szamla)):
+    if szamla[i] > 0:
+        pozitiv_ertekek.append(szamla[i])
+legkisebb_pozitiv = min(pozitiv_ertekek)
+print("Legkisebb jóváírás:", legkisebb_pozitiv)
 
-for  i in range(len(számla)):
-    if számla[i] > 100:
-        a += számla[i]
+atlag = 0
+pozitiv_db = 0
+for i in range(len(szamla)):
+    if szamla[i] > 0:
+        atlag += szamla[i]
+        pozitiv_db += 1
+atlag = atlag / pozitiv_db
+print(f"Pozitív átlag: {atlag:.2f}")
 
-print(f'{a} esetben utaltak legalább száz eurót a számlára')
-
-v = []
-k = []
-
-for  i in range(len(számla)):
-    if számla[i] > 0:
-        k.append(számla[i])
-    elif számla[i] < 0:
-        v.append(számla[i])
-print(f'{max(v)} a legnagyobb levonás, {max(k)} a legnagyobb utalás {min(számla)} a legkisebb utalás')
-
-ind = 0
-
-for  i in range(len(számla)):
-    if számla[i] == 225:
-        ind=i
-print(f'A {ind}. sorszámú utalás volt 225 euro')
-
-for i in range(len(k)):
-    if számla[i] == 225:
-        ind=i
-print(f'A {ind}. sorszámú utalás volt 225 euro ha nem számítjuk a levonásokat')
-
-
-for i in range(len(k)):
-    if számla[i] > 1000:
-        print(f'Az 1000-nél nagyobb utalás az {i}. volt a listában')
-else: print(f'Az 1000-nél nagyobb utalás nem volt a listában')
-
-
-for  i in range(len(számla)):
-    if számla[i] %10 ==0 :
-        print('Van olyan összeg amit ki lehet venni bankautomatából')
-else: print('Nincs olyan összeg amit ki lehet venni bankautomatából')
-
-
+osszeg_uj = 0
+for i in range(len(szamla)):
+    osszeg_uj += szamla[i]
+if osszeg_uj > 0:
+    print("A számla pozitív egyenlegű")
+elif osszeg_uj < 0:
+    print("A számla negatív egyenlegű")
+else:
+    print("A számla egyenlege nulla")
