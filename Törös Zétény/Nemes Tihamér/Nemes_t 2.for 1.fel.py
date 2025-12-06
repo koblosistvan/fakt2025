36c9c100496137d99c909d035111de4a92bceaa3
Tejbep <toros.zeteny@gmail.com>
2025-12-06 14:16:48 +0100

    létra

--- Törös Zétény/Nemes Tihamér/Nemes_t 2.for 1.fel.py
+++ Törös Zétény/Nemes Tihamér/Nemes_t 2.for 1.fel.py
@@ -1,5 +1,5 @@
-csokik = [int(input())]
-if csokik[0] + csokik[1] == csokik[2] + csokik[3] or csokik[0] + csokik[2] == csokik[1] + csokik[3] or csokik[0] + csokik[3] == csokik[2] + csokik[1]:
+csokik = list(map(int,input().split()))
+if csokik[0] + csokik[1] == csokik[2] + csokik[3] or csokik[0] + csokik[2] == csokik[1] + csokik[3] or csokik[0] + csokik[3] == csokik[1] + csokik[2] or csokik[0] + csokik[1] + csokik[2] == csokik[3] or csokik[0] + csokik[1] + csokik[3] == csokik[2] or csokik[0] + csokik[2] + csokik[2] == csokik[1] or csokik[3] + csokik[1] + csokik[2] == csokik[0]:
     print("IGEN")
 else:
     print("NEM")
