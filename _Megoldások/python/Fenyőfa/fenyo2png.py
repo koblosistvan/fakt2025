dd554182c853d7130179aa3e3ea6def8a33addea
koblosistvan <koblos.istvan@consense.hu>
2025-12-09 12:56:04 +0100

    fenyőfa

---
+++ _Megoldások/python/Fenyőfa/fenyo2png.py
@@ -0,0 +1,26 @@
+from PIL import Image
+
+source = open('Fenyőfa\\fenyofa.txt', 'r', encoding='utf-8')
+width, height = [int(a) for a in source.readline().strip().split()]
+pixels = []
+for _ in range(height):
+    line = source.readline()
+    pixels.append(line.rstrip('\n'))
+
+COLORS = {                     # RGBA, az utolsó az átlátszóság
+    'h': (0, 0, 0, 0),         # átlátszó háttér
+    'z': (0, 150, 0, 255),     # zöld
+    'b': (120, 72, 0, 255),    # barna
+    's': (255, 215, 0, 255),   # sárga
+    'p': (220, 20, 60, 255),   # piros
+    'k': (30, 144, 255, 255),  # kék
+    'f': (255, 255, 255, 255)  # fehér
+}
+
+img = Image.new('RGBA', (width, height), (0, 0, 0, 0)) 
+for y in range(height):
+    for x in range(width):
+        img.putpixel((x, y), COLORS[pixels[y][x]])
+
+img.save('Fenyőfa\\fenyofa.png')
+print('Elkészült a képfájl.')
---
+++ _Megoldások/python/Fenyőfa/fenyofa.py
@@ -0,0 +1,14 @@
+'''
+Adott egy kép egy fenyőfáról, melynek minden pixelét egy karakter jelöl a forrás állományban
+Méretek: 40x40 pixel, soronként balról jobbra haladva.
+Jelmagyarázat: k=fekete háttér, Z=zöld, B=barna, s=sárga, p=piros, b=kék, f=fehér
+
+Feladatok:
+1. olvasd be és tárold el a kép karaktereit
+2. hány hópihét látsz a képen?
+3. melyik sorban van a törzs teteje?
+4. melyik sorban van a törzs alja, milyen magas a fa törzse?
+5. hány dísz van a fán? 
+6. milyen színű díszeket látsz, melyikből hányat?
+'''
+
