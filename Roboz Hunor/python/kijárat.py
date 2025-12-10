6f23d2b8eda5b9e7961ca821cfb4f6f63cf151f6
RobozHunor <robozhunor@gmail.com>
2025-12-10 14:41:37 +0100

    ördögszekér

--- Roboz Hunor/python/kijárat.py
+++ Roboz Hunor/python/kijárat.py
@@ -1,4 +1,4 @@
-forras = open('_Megoldások\\Szakkör\\Labirintus\\labirintus.txt',mode='r',encoding='utf-8')
+forras = open('Roboz Hunor\\python\\labirintus.txt',mode='r',encoding='utf-8')
 adat = forras.readline().split()
 magasság,szélesseg = int(adat[0]),int(adat[1])
 
@@ -9,3 +9,30 @@ for _ in range(magasság):
 
 print('\n'.join(labirintus))
 
+graf = {}
+szomszédok = [
+    (-1,0),
+    (0,-1),
+    (0,1),
+    (1,0)
+]
+
+
+def uj_el(a,b):
+    if a in graf:
+        graf[a].append(b)
+    else:
+        graf[a] = [b]
+
+
+
+for sor in range(1, magasság-1):
+    for oszlop in range (1, szélesseg-1):
+        for sz in szomszédok:
+            én = labirintus [sor][oszlop]
+            szomszed = labirintus [sor+sz[0]][oszlop+sz[1]]
+            if én == '.' and szomszed == '.':
+                uj_el(sor*10+oszlop,(sor+sz[0])*10 +oszlop +sz[1])
+
+
+print(graf)
