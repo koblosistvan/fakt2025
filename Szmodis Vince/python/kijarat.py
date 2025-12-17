ab2a55f0efe02f7f03d2aeb5600cc0828131834c
Szmodis Vince <vince.szmodis@gmai.com>
2025-12-17 14:25:39 +0100

    .

---
+++ Szmodis Vince/python/kijarat.py
@@ -0,0 +1,55 @@
+FAL = '#'
+UT = ' '
+
+forras = open('_Megoldások\\Szakkör\\Labirintus\\labirintus-42-41x41.txt', mode='r', encoding='utf-8')
+adat = forras.readline().strip().split()
+magassag, szelesseg = int(adat[0]), int(adat[1])
+
+adat = forras.readline().strip().split()
+kijarat = ( int(adat[0]), int(adat[1]) )
+
+adat = forras.readline().strip().split()
+start = ( int(adat[0]), int(adat[1]) )
+
+labirintus = []
+for _ in range(magassag):
+    labirintus.append(forras.readline().strip())
+
+print('\n'.join(labirintus))
+
+'''
+graf = {
+    '1': [2, 3, 4],
+    '2': [1, 4, 7]
+}
+'''
+
+graf = {}
+szomszedok = [
+    (-1, 0),
+    (0, -1),
+    (0, 1),
+    (1, 0),
+]
+
+
+def uj_el(a, b):
+    if a not in graf:
+        graf[a] = []
+    graf[a].append(b)
+    
+
+for sor in range(1, magassag-1):
+    for oszlop in range(1, szelesseg-1):
+        # végigmegyünk a szomszédokon
+        for sz in szomszedok:
+            én = labirintus[sor][oszlop]
+            szomszed = labirintus[sor + sz[0]][oszlop + sz[1]]
+            # én is UT vagyok és a szomszéd is, akkor élt találtam
+            if én == UT and szomszed == UT:
+                uj_el((sor, oszlop), (sor + sz[0], oszlop + sz[1])) 
+
+print(graf)
+
+tavolsag ={csucs:1000 for csucs in graf}
+elozo={csucs: None for csucs in graf}
