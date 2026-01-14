4d2fbf84226d8f3f80f245476d6295b95a0a3453
Sipos Kristóf <kristof.sipos@gmail.com>
2026-01-14 10:53:04 +0100

    alama

---
+++ SIpos Kristóf/Rendes óra/python 1.py
@@ -0,0 +1,4 @@
+oldal_1= float(input('Add meg a négyszög egyik oldalát: '))
+oldal_2= float(input('Add meg a másik oldalát: '))
+kerület= float(2*(oldal_1+oldal_2))
+print(f'A négyszöged kerülete {kerület} cm')
