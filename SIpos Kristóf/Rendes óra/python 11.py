e61222e284256ccabd9b8fc42401660c4cfef967
Sipos Kristóf <kristof.sipos@gmail.com>
2026-01-21 10:50:02 +0100

    3

---
+++ SIpos Kristóf/Rendes óra/python 11.py
@@ -0,0 +1,4 @@
+tömeg=float(input('Add meg a tömeget: '))
+gravitációs_gyorsulás=float(input('Add meg a gravitációs gyorsulást m/s^2-ben: '))
+erő=tömeg*gravitációs_gyorsulás
+print(f'Az erő {erő:.2f}.')
