d4b41ba3bac8a5346a9aa0512c0a137c9b69118e
odora23 <odorartur09@gmail.com>
2026-05-27 09:50:57 +0200

    jack

--- Ódor Artúr/python/class/black_jack.py
+++ Ódor Artúr/python/class/black_jack.py
@@ -22,13 +22,14 @@ class Cards:
 
 class Deck:
     def __init__(self):
+        self.cards = []
         for s in VAULE:
             for c in COLOR:
                 self.cards.append(Cards(vaule = s, color = c))
 
 
     def __repr__(self):
-        return 
+        return f"{len(self.cards)}/52 lap"
 
 
     def shuffle(self):
@@ -49,27 +50,83 @@ class Hand:
         self.name = name
         self.cards = []
 
-    
+
+    def __repr__(self):
+        return f"{self.vaule()} {self.cards} "
+
+
     def add_cards(self, c: Cards):
         self.cards.append(c)
 
 
     def vaule(self):
         v = 0
+        a_count = 0
         for c in self.cards:
             if c.vaule in ("A"):
-                v +=11
+                v += 11
+                a_count += 1
             elif c.vaule in ("J", "D", "K", "10"):
                 v += 10
             else:
                 v += int(c.vaule)
+        
+        sub_a = min((v-12)//10, a_count)
 
+        return v - sub_a*10 if sub_a > 0 else v
+jatek = True
 
+while jatek:
+    d = Deck()
+    d.shuffle()
+    print(d)
+    oszto = Hand(name="Dealer")
+    player = Hand(name="Kertész József")
+    for _ in range(2):
+        oszto.add_cards(c= d.draw())
+        player.add_cards(c= d.draw())
+    print("Osztó:", oszto.cards[0])
+    print("Játékos:", player)
 
-kártya = Cards("kör","2")
-print(kártya)
+    if oszto.vaule() == 21:
+        winner = oszto
+    else:
+        player_draws = input("Kérsz még lapot? (i/n): ")
 
-d = Deck()
-print(d)
-c = d.draw
-print(c)
+        while player_draws == "i":
+            player.add_cards(c=d.draw())
+            print("Játékos:", player)
+            if player.vaule() > 21:
+                break
+            player_draws = input("Kérsz még lapot? (i/n): ")
+            
+
+        winner  = None
+
+        if player.vaule() > 21:
+            winner = oszto
+
+        else:
+            while oszto.vaule() <17:
+                oszto.add_cards(c= d.draw())
+                print("Dealer:",oszto)
+
+            if player.vaule() > oszto.vaule() or oszto.vaule() > 21:
+                winner = player
+            elif player.vaule() < oszto.vaule():
+                winner = oszto
+
+    print("A játéknak vége.")
+
+    print("Dealer:",oszto)
+    print("Játékos:",player)
+    if winner:
+        print(f"A győztes: {winner.name}")
+    else:
+        print("Döntetlen")
+
+    uj_kor = input("Akarsz újra játszani? (i/n): ")
+    if uj_kor == "i":
+        jatek = True
+    else:
+        jatek = False
